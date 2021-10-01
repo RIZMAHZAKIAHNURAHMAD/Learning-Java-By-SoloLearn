dad5ba67575de7f26cb6dcbdb2e8f29b35625875
Rizmah Zakiah Nur Ahmad <91116778+RIZMAHZAKIAHNURAHMAD@users.noreply.github.com>
2021-10-01 14:39:58 +0700

    Basic Concepts
    A programmer wrote a program to print odd numbers. He wanted to explain how the program works in the code, 
    but he forgot to comment and the program does not work. Comment lines so that it prints the odd numbers.

---
+++ PrintingNumber.java
@@ -0,0 +1,11 @@
+public class PrintingNumber {
+
+   public static void main(String[] args) {
+       /*So the program prints some odd
+       numbers and it works perfectly*/
+	   
+       System.out.println("1");
+       System.out.println("3");
+       System.out.println("7");
+   }
+}
