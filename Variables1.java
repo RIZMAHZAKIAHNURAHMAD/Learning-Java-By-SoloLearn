3fe70e08589431712bff2a849c27ff53945aef9a
Rizmah Zakiah Nur Ahmad <91116778+RIZMAHZAKIAHNURAHMAD@users.noreply.github.com>
2021-10-01 14:42:37 +0700

    Basic Concepts
    Drag and drop from the options below to have a valid Java program.
    
    (Pertanyaan drag and drop di Java Sololearn)

---
+++ Variables1.java
@@ -0,0 +1,13 @@
+class Variables1 {
+
+  public static void main(String[ ]args) {
+
+     String name = "John";
+     int age = 24;
+     double height = 189.87;
+	 
+	 System.out.println("Name:" + name);
+	 System.out.println("Age:" + age);
+	 System.out.println("Height:" + height);
+  }
+}
