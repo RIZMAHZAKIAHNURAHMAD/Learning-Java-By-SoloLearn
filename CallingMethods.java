74eedf6d3f0abdf1b9369ffce68ebf7ac9853429
Rizmah Zakiah Nur Ahmad <91116778+RIZMAHZAKIAHNURAHMAD@users.noreply.github.com>
2021-10-04 01:04:30 +0700

    Classes and Objects
    You can call a method as many times as necessary.
    When a method runs, the code jumps down to where the method is defined, executes the code inside of it, then goes back and proceeds to the next line.
    Example:

---
+++ CallingMethods.java
@@ -0,0 +1,11 @@
+class CallingMethods {
+	static void sayHello() {
+		System.out.println("Hello World");
+	}
+	
+	public static void main (String[] args) {
+		sayHello();
+		sayHello();
+		sayHello();
+	}
+}
