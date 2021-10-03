fd80dbb5d28b7ce6b2aadeeec102ad6670477d10
Rizmah Zakiah Nur Ahmad <91116778+RIZMAHZAKIAHNURAHMAD@users.noreply.github.com>
2021-10-03 21:47:26 +0700

    Conditional and Loops
    Contoh 2 while loops.  When the expression is tested and the result is false, the loop body is skipped and the first statement after the while loop is executed.

---
+++ WhileLoops2.java
@@ -0,0 +1,20 @@
+class WhileLoops2 {
+	public static void main(String[] args) {
+		int x = 6;
+		
+		while(x < 10) 
+		{
+			System.out.println(x);
+			x++;
+		}
+		System.out.println("Loop Ended");
+		
+		/*
+		6
+		7
+		8
+		9
+		Loop ended
+		*/
+	}
+}
